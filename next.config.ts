dd33c6491fdbc58c4861e7abc5f82adb4bc06c0c
Kapil Sharma <kapil19092003@gmail.com>
2024-11-17 13:38:16 +0530

    code

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
+const port = process.env.PORT || 3000;
+
+module.exports = {
+  serverRuntimeConfig: {
+    port,
+  },
+};
 
 export default nextConfig;
